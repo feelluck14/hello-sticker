55844b0dbe83441507d628fe494077581dfa9b2f
feelluck14 <feelluck@nextsingular.com>
2025-12-29 16:44:39 +0900

    캐시 사용 안하게 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // 모든 경로에 적용
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store", // 캐시 사용하지 않음
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
